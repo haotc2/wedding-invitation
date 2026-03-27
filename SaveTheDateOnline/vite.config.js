7730cfc0ee468b2ed2d6e86403359ebcc066d91d
haotc2 <42366432+haotc2@users.noreply.github.com>
2026-03-27 23:40:04 +0700

    update code

--- SaveTheDateOnline/vite.config.js
+++ SaveTheDateOnline/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SaveTheDateOnline/', // Tên repo trên GitHub
+  base: '/wedding-invitation/', // Tên repo trên GitHub
 })
